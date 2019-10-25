f806adf94a7a2f378ed810899e4143f894aafb18
Waqas Suriya <waqassuriya4@gmail.com>
2019-10-25 17:23:49 +0500

    backend

--- index.js
+++ index.js
@@ -16,10 +16,18 @@ const app = express();
 //   res.send("Hello Express app!");
 // });
 
-app.get("/download", (req, res) => {
+app.get("/download", (req, res, next) => {
   const url = req.query.url;
+  if (!url) {
+    next(new Error("Missing url"));
+    return;
+  }
+
   youtubedl.getInfo(url, function(err, info) {
-    if (err) throw err;
+    if (err) {
+      next(new Error("Lookup failed"));
+      return;
+    }
 
     console.log("id:", info.id);
     console.log("title:", info.title);
@@ -34,6 +42,11 @@ app.get("/download", (req, res) => {
 });
 
 app.use(express.static("dist"));
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message || "Something broke!");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
